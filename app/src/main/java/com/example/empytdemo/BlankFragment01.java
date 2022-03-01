5b2aabed6e588fad5941cdc0ca654d0bbe57d3d9
zhuyi_y7000 <1739677792@qq.com>
2022-03-01 22:13:01 +0800

    增加了TestFragmentActivity，可以动态添加/切换fragment，测试了原生的（bundle）activity到fragment的消息传递

--- app/src/main/java/com/example/empytdemo/BlankFragment01.java
+++ app/src/main/java/com/example/empytdemo/BlankFragment01.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 //调用Fragment的Activity继承的父类不能使用Activity，要继承 FragmentActivity 或者 AppCompatActivity
@@ -23,6 +25,11 @@ public class BlankFragment01 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //测试消息传递
+        Bundle bundle = this.getArguments();//在这个类的任何位置都可以访问这个方法获取到传递来的信息
+        String test_str = bundle.getString("testMessage");
+        Log.e("zhu","传递来的消息是："+test_str);
+
     }
 
     @Override
@@ -31,6 +38,7 @@ public class BlankFragment01 extends Fragment {
         if(root_view == null) {
             root_view = inflater.inflate(R.layout.fragment_blank01, container, false);
         }
+        Log.e("zhu","创建blankFragment01");
         textView = root_view.findViewById(R.id.fragment01_textview);
         button = root_view.findViewById(R.id.fragment01_btn);
 
@@ -40,7 +48,6 @@ public class BlankFragment01 extends Fragment {
                 textView.setText("你好~");
             }
         });
-
         return root_view;
     }
 }
--- app/src/main/java/com/example/empytdemo/TestFragmentActivity.java
+++ app/src/main/java/com/example/empytdemo/TestFragmentActivity.java
@@ -1,14 +1,61 @@
 package com.example.empytdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TestFragmentActivity extends AppCompatActivity {
+public class TestFragmentActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fragment);
+
+        Button button01 = findViewById(R.id.test_fragment_btn1);
+        button01.setOnClickListener(this);
+
+        Button button02 = findViewById(R.id.test_fragment_btn2);
+        button02.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.test_fragment_btn1:
+                //以bundle的形式传递信息
+                Bundle bundle = new Bundle();
+                bundle.putString("testMessage","Activity到Fragment的信息传递");
+
+                BlankFragment01 bf01 = new BlankFragment01();
+                bf01.setArguments(bundle);//传递到fragment中
+
+                replaceFragment(bf01);//再动态创建这个fragment
+                break;
+            case R.id.test_fragment_btn2:
+                replaceFragment(new BlankFragment02());
+                break;
+        }
+    }
+
+    /*
+    * 1.创建一个待处理的fragment
+    * 2.获取FragmentManager，一般通过getSupportFragmentManager()
+    * 3.开启一个事务transaction，一般调用FragmentManager的beginTransaction();
+    * 4.使用transaction进行fragment的替换（可以选择入栈）
+    * 5.提交事务 commit
+    * */
+
+    //replace方法,动态切换fragment
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();//通过manager获取触发器
+        transaction.replace(R.id.test_fragment_framelayout,fragment);
+        transaction.addToBackStack(null);  //将fragment入栈，在用户点击返回键时，逐个出栈
+        transaction.commit();
     }
 }
